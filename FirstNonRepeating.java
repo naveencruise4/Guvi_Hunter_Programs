d95f4687645f25f55277b78fec2a81c05c9748dd
naveencruise4 <fearlessnaveen4@gmail.com>
2016-08-22 17:39:15 +0530

    Add files via upload

--- FirstNonRepeating.java
+++ FirstNonRepeating.java
@@ -1,25 +1,29 @@
 package Logical2Programs;
 
+import java.util.Scanner;
+
 public class FirstNonRepeating {
 public static void main(String[] args) {
+	System.out.println("Enter Numbers");
+	Scanner s=new Scanner(System.in);
+	String s1=s.next();
+String str[]=s1.split("");
 	
-	int a[]={10, 5, 3, 4, 3, 5, 6};
-	int l=a.length;
+	int l=str.length;
 		int temp=0;
 		for(int i=0;i<l;i++)
 	{
 			temp=0;
 			for(int j=0;j<l;j++)
 			{
-				if(a[i]==a[j])
-				{
+				if(Integer.parseInt(str[i])==Integer.parseInt(str[j]))
 					temp+=1;
 				}
-			}
+			
 			if(temp==1)
 			{
-				System.out.println(a[i]);
-				break;
+				System.out.println(Integer.parseInt(str[i]));
+			break;
 			}
 					}
 		
